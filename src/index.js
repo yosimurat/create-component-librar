41c81859d3d81c4a4625816c49d6a11ab6709552
Toshiyuki Yoshimura <yosimurat@gmail.com>
2018-08-15 16:53:42 +0900

    Build distribution package and declar default export

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from "react-dom";
-import { TextInput } from "./lib";
+import TextInput  from "./lib";
 
 const App = () => (
   <div style={{ width: 640, margin: "15px auto" }}>
